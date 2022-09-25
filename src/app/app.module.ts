0a4ee8e4c27eb92650adca06f16cbf783e91c3be
Sandro <sandrito_2003@hotmail.com>
2022-09-25 12:59:26 -0500

    Footer Inicio added | Sandro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule} from '@angular/common/http';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainFooterComponent } from './common/main-footer/main-footer.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { WhatwedoComponent } from './home/whatwedo/whatwedo.component';
+import { BlackwhiteComponent } from './home/blackwhite/blackwhite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFooterComponent,
+    CarouselComponent,
+    InicioComponent,
+    WhatwedoComponent,
+    BlackwhiteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/blackwhite/blackwhite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blackwhite',
+  templateUrl: './blackwhite.component.html',
+  styleUrls: ['./blackwhite.component.css']
+})
+export class BlackwhiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
